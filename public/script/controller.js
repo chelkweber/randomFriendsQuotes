253a0574ba4b4eac8ef286cf745fa7abadce4a8b
Chelsea Weber <chelseaweber92@gmail.com>
2017-05-15 11:39:01 -0400

    all characters quotes

--- public/script/controller.js
+++ public/script/controller.js
@@ -3,11 +3,46 @@ var app = angular.module('quoteMod');
 app.controller('quoteCtrl', function($scope, quoteService) {
 	console.log('controller has loaded');
 	
-	$scope.quote = {};
+	$scope.rachelQuote = {};
 	
-	quoteService.getQuote().then(function(quote) {
-		$scope.quote = quote;
-		console.log($scope.quote);
+	quoteService.getRachelQuote().then(function(quote) {
+		$scope.rachelQuote = quote;
+		console.log($scope.rachelQuote);
+	});
+	
+	$scope.rossQuote = {};
+	
+	quoteService.getRossQuote().then(function(quote) {
+		$scope.rossQuote = quote;
+		console.log($scope.rossQuote);
+	});
+	
+	$scope.monicaQuote = {};
+	
+	quoteService.getMonicaQuote().then(function(quote) {
+		$scope.monicaQuote = quote;
+		console.log($scope.monicaQuote);
+	});
+	
+	$scope.chandlerQuote = {};
+	
+	quoteService.getChandlerQuote().then(function(quote) {
+		$scope.chandlerQuote = quote;
+		console.log($scope.chandlerQuote);
+	});
+	
+	$scope.phoebeQuote = {};
+	
+	quoteService.getPhoebeQuote().then(function(quote) {
+		$scope.phoebeQuote = quote;
+		console.log($scope.phoebeQuote);
+	});
+	
+	$scope.joeyQuote = {};
+	
+	quoteService.getJoeyQuote().then(function(quote) {
+		$scope.joeyQuote = quote;
+		console.log($scope.joeyQuote);
 	});
 	
 });
--- public/script/service.js
+++ public/script/service.js
@@ -4,12 +4,47 @@ app.factory('quoteService', function($http) {
 	var quote = {};
 	
 	return {
-		getQuote: function() {
+		getRachelQuote: function() {
 			return $http.get('/rachel', function(data) {
 				quote = data;
 				return quote;
 			});
-		}
+		},
+		
+		getRossQuote: function() {
+			return $http.get('/ross', function(data) {
+				quote = data;
+				return quote;
+			});
+		},
+		
+		getMonicaQuote: function() {
+			return $http.get('/monica', function(data) {
+				quote = data;
+				return quote;
+			});
+		},
+		
+		getChandlerQuote: function() {
+			return $http.get('/chandler', function(data) {
+				quote = data;
+				return quote;
+			});
+		},
+		
+		getPhoebeQuote: function() {
+			return $http.get('/phoebe', function(data) {
+				quote = data;
+				return quote;
+			});
+		},
+		
+		getJoeyQuote: function() {
+			return $http.get('/joey', function(data) {
+				quote = data;
+				return quote;
+			});
+		},
 	}
 	
 })
