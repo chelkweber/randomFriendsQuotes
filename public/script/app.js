49474689d278f239bd828bdccc3fc1abc29e5a11
Chelsea Weber <chelseaweber92@gmail.com>
2017-05-15 11:50:10 -0400

    ng-route

--- public/script/app.js
+++ public/script/app.js
@@ -1,3 +1,27 @@
 console.log('Module is working');
 
-var app = angular.module('quoteMod', []);
+var app = angular.module('quoteMod', ['ngRoute']);
+
+app.config(function($routeProvider) {
+	$routeProvider.when('/rachel', {
+		templateUrl: 'views/rachel.html'
+	})
+	.when('/ross', {
+		templateUrl: 'views/ross.html'
+	})
+	.when('/monica', {
+		templateUrl: 'views/monica.html'
+	})
+	.when('/chandler', {
+		templateUrl: 'views/chandler.html'
+	})
+	.when('/phoebe', {
+		templateUrl: 'views/phoebe.html'
+	})
+	.when('/joey', {
+		templateUrl: 'views/joey.html'
+	})
+	.otherwise({
+		template: 'Pick a Friend!'
+	})
+});
