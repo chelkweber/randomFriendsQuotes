741c7a73fe3d06d2acd9458c6a9c5b5d643dc193
Chelsea Weber <chelseaweber92@gmail.com>
2017-05-15 10:46:09 -0400

    pubilc folder and front-end file structure added

--- server.js
+++ server.js
@@ -2,40 +2,45 @@ var express = require('express');
 var app = express();
 var data = require('./data');
 
+//connect to public folder
+app.use(express.static(__dirname + '/public'))
+
+//get request to folder
+
 app.get('/', function(request, response) {
 	response.send('working')
 });
 
 //random quotes
 
-app.get('/random/rachel', function(request, response) {
+app.get('/rachel', function(request, response) {
 	response.send(data.characters.rachel.quotes[Math.floor(Math.random() * data.characters.rachel.quotes.length)]);
 });
 
-app.get('/random/ross', function(request, response) {
+app.get('/ross', function(request, response) {
 	response.send(data.characters.ross.quotes[Math.floor(Math.random() * data.characters.ross.quotes.length)]);
 });
 
-app.get('/random/monica', function(request, response) {
+app.get('/monica', function(request, response) {
 	response.send(data.characters.monica.quotes[Math.floor(Math.random() * data.characters.monica.quotes.length)]);
 });
 
-app.get('/random/chandler', function(request, response) {
+app.get('/chandler', function(request, response) {
 	response.send(data.characters.chandler.quotes[Math.floor(Math.random() * data.characters.chandler.quotes.length)]);
 });
 
-app.get('/random/phoebe', function(request, response) {
-	response.send(data.characters.pheobe.quotes[Math.floor(Math.random() * data.characters.phoebe.quotes.length)]);
+app.get('/phoebe', function(request, response) {
+	response.send(data.characters.phoebe.quotes[Math.floor(Math.random() * data.characters.phoebe.quotes.length)]);
 });
 
-app.get('/random/joey', function(request, response) {
+app.get('/joey', function(request, response) {
 	response.send(data.characters.joey.quotes[Math.floor(Math.random() * data.characters.joey.quotes.length)]);
 });
 
 //all quotes
 
-app.get('/all/rachel', function(request, response) {
-	response.send(data.characters.rachel.quotes);
+app.get('/all', function(request, response) {
+	response.send(data.characters);
 })
 
 
@@ -44,6 +49,5 @@ app.get('/all/rachel', function(request, response) {
 var server = app.listen(8080, function() {
 	var port = server.address().port;
 	console.log('working', port);
-	console.log(data.characters);
 });
 
