01d9bf583f19413626ee13b7a6db2cf324ae72d8
Chelsea Weber <chelseaweber92@gmail.com>
2017-05-15 22:36:57 -0400

    additonal quotes

--- data.js
+++ data.js
@@ -1,32 +1,32 @@
 var characters= {
 	rachel: {
 		'name': 'Rachel Green',
-		'quotes': ["How long do cats live? Like assuming you don’t throw them under a bus or something?", "Your fly is open, Geller!", "Just so you know, it's not that common, it doesn't happen to every guy, and it is a big deal!", "He's a transponster!", "It’s a trifle. It’s got all of these layers. First there’s a layer of ladyfingers, then a layer of jam, then custard, which I made from scratch, then raspberries, more ladyfingers, then beef sauteed with peas and onions, then a little more custard, and then bananas, and then I just put some whipped cream on top!" ],
+		'quotes': ["How long do cats live? Like assuming you don’t throw them under a bus or something?", "Your fly is open, Geller!", "Just so you know, it's not that common, it doesn't happen to every guy, and it is a big deal!", "He's a transponster!", "It’s a trifle. It’s got all of these layers. First there’s a layer of ladyfingers, then a layer of jam, then custard, which I made from scratch, then raspberries, more ladyfingers, then beef sauteed with peas and onions, then a little more custard, and then bananas, and then I just put some whipped cream on top!", "It's like there's rock bottom, then 50 feet of crap, then me.", "I mean, isn't that just kick-you-in-the-crotch, spit-on-your-neck fantatsic?", "I'm so happy and not at all jealous" ],
 		'id': 1
 	},
 	ross: {
 		'name': 'Ross Geller',
-		'quotes': ["We were on a break!", "Pivot! Pivot! Pivot! Pivot. Pivot. Pivot.", "I grew up in a house with Monica, okay. If you didn't eat fast, you didn't eat."],
+		'quotes': ["We were on a break!", "Pivot! Pivot! Pivot! Pivot. Pivot. Pivot.", "I grew up in a house with Monica, okay. If you didn't eat fast, you didn't eat.", "Unagi", "When I actually die some people are gonna get seriously haunted,", "I'm fine!", "Why do bad things happen to good people?", "Ross: The Divorce Force", "You threw my sandwich away?", "Mississippilessly?", "Y-O-U'R-E means you ar. Y-O-U-R means your!"],
 		'id': 2
 	},
 	monica: {
 		'name': 'Monica Geller',
-		'quotes': ["Judge all you want to but, [points to Ross] married a lesbian, [points to Rachel] left a man at the altar, [points to Phoebe] fell in love with a gay ice dancer, [points to Joey] threw a girl's wooden leg in a fire, [points to the box that Chandler is in] live in a box!"], 
+		'quotes': ["Judge all you want to but, [points to Ross] married a lesbian, [points to Rachel] left a man at the altar, [points to Phoebe] fell in love with a gay ice dancer, [points to Joey] threw a girl's wooden leg in a fire, [points to the box that Chandler is in] live in a box!", "And remeber, if I'm harsh with you it's only because you're doing it wrong", "I'm breezy!", "Damn all the jellyfish!", "That's not even a word!", "I know! I know! I know!", "Welcome to the real world! It sucks. You're gonna love it."], 
 		'id': 3
 	},
 	chandler: {
 		'name': 'Chandler Bing',
-		'quotes': ["Gum would be perfection.", "I can handle this. Handle is my middle name. Actually, handle is the middle of my first name.", "I'm not so good with the advice... Can I interest you in a sarcastic comment?", "[to Ross] Three failed marriages, two illegitimate children... The personal ad writes itself."],
+		'quotes': ["Gum would be perfection.", "I can handle this. Handle is my middle name. Actually, handle is the middle of my first name.", "I'm not so good with the advice... Can I interest you in a sarcastic comment?", "[to Ross] Three failed marriages, two illegitimate children... The personal ad writes itself.", "All right, I took the quiz. And it turns out I do put career before men.", "It's a Sunday. I don't move on Sundays.", "Hi, I'm Chandler. I make jokes when I'm uncomforatable.", "I'm hopeless and awkward and desperate for love!"],
 		'id': 4
 	}, 
 	phoebe: {
 		'name': 'Phoebe Buffay',
-		'quotes': ["Smelly cat, smel-ly cat, what are they feeding you? Smelly cat, smel-ly cat, it's not your fault.", "But they don't know that we know they know we know!", "If you want to receive e-mails about my upcoming shows, then please give me money so I can buy a computer."],
+		'quotes': ["Smelly cat, smel-ly cat, what are they feeding you? Smelly cat, smel-ly cat, it's not your fault.", "But they don't know that we know they know we know!", "If you want to receive e-mails about my upcoming shows, then please give me money so I can buy a computer.", "I have to go before I put your head through a wall.", "[Tag: 'Phoebe. Great Name'] You like that? You should hear my phone number.", "Come on, Ross. You're a paleontologist. Dig a little deeper.", "This is brand new information!", "Pick up the sock!", "Oh, I wish I could, but I don't want to."],
 		'id': 5
 	}, 
 	joey: {
 		'name': 'Joey Tribianni',
-		'quotes': ["How you doin'?!", "I like it. What's not to like? Custard? Good. Jam? Good. Meat? Good.", "If he doesn't like you this is all a moo point. ... Yeah, it's like a cow's opinion. It just doesn't matter. It's moo.", "Here come the meat sweats.", "Look at me! I'm Chandler! Could I be wearing any more clothes?!" ], 
+		'quotes': ["How you doin'?!", "I like it. What's not to like? Custard? Good. Jam? Good. Meat? Good.", "If he doesn't like you this is all a moo point. ... Yeah, it's like a cow's opinion. It just doesn't matter. It's moo.", "Here come the meat sweats.", "Look at me! I'm Chandler! Could I be wearing any more clothes?!", "[to Ross] Occupation...? Dinosaurs", "Well, the fridge broke, so I had to eat everything.", "You hung up on the pizza place? I don't hang up on your friends.", "You can't just give up! Is that what a dinosaur would do?" ], 
 		'id': 6
 	}
 }
